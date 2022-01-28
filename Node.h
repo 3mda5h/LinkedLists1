357a8709bfec2752768520567115765997f1e5b0
3mda5h <90864557+3mda5h@users.noreply.github.com>
2022-01-27 19:02:55 -0800

    Add files via upload

---
+++ Node.h
@@ -0,0 +1,24 @@
+//set up the getters and setters for student and next pointers, constructor/destructor; linked with Student.h to use student stuff
+
+#ifndef NODE_H
+#define NODE_H
+#include <iostream> 
+#include "Student.h"
+
+using namespace std;
+
+class Node {
+  public:
+    Node(Student* newStudent);
+    ~Node();
+    Node* getNext();
+    void setNext(Node* newNext);
+    Student* getStudent();
+
+  //student and next pointer variables
+  private:
+    Student* student;
+    Node* next;
+};
+
+#endif
